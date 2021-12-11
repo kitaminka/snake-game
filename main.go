7a65ab391ca287949526c0ebf11fa330c0209936
Kitaminka <kit4minka@outlook.com>
2021-12-11 13:13:59 +0200

    Create gameCycle

--- main.go
+++ main.go
@@ -14,12 +14,13 @@ type Cell struct {
 type Snake struct {
 	head      Cell
 	tail      []Cell
-	field     Field
+	field     *Field
 	style     tcell.Style
 	direction struct {
 		x int
 		y int
 	}
+	delay time.Duration
 }
 type Field struct {
 	x      int
@@ -28,7 +29,7 @@ type Field struct {
 	height int
 	style  tcell.Style
 	cells  []Cell
-	screen tcell.Screen
+	screen *tcell.Screen
 }
 
 func main() {
@@ -47,46 +48,57 @@ func main() {
 	//appleStyle := tcell.StyleDefault.Background(tcell.ColorDefault).Foreground(tcell.ColorRed)
 
 	drawText(s, width/2-5, 1, width/2+5, 1, defStyle, "Snake Game")
-	f := newField(s, width/2-50, 3, 100, 12, defStyle)
-	f.DrawField()
 
+	f := newField(&s, width/2-50, 3, 100, 12, defStyle)
 	snake := newSnake(f, f.x+f.width/2, f.y+f.height/2, 5, snakeStyle)
-	snake.DrawSnake()
+
+	ch := make(chan bool)
+
 	go animationCycle(&snake)
+	go gameCycle(&snake, ch)
+
+	<-ch
+
+	s.Fini()
+	os.Exit(0)
+}
+
+func gameCycle(snake *Snake, ch chan bool) {
 	for {
+		s := *snake.field.screen
+
 		ev := s.PollEvent()
 
 		switch ev := ev.(type) {
 		case *tcell.EventResize:
 			s.Sync()
 		case *tcell.EventKey:
-			if ev.Key() == tcell.KeyCtrlC {
-				s.Fini()
-				os.Exit(0)
-			} else if ev.Key() == tcell.KeyUp {
-				snake.direction.y = -1
-				snake.direction.x = 0
-			} else if ev.Key() == tcell.KeyDown {
+			switch ev.Key() {
+			case tcell.KeyCtrlC:
+				ch <- true
+			case tcell.KeyDown:
 				snake.direction.y = 1
 				snake.direction.x = 0
-			} else if ev.Key() == tcell.KeyRight {
+			case tcell.KeyUp:
+				snake.direction.y = -1
+				snake.direction.x = 0
+			case tcell.KeyRight:
 				snake.direction.y = 0
 				snake.direction.x = 1
-			} else if ev.Key() == tcell.KeyLeft {
+			case tcell.KeyLeft:
 				snake.direction.y = 0
 				snake.direction.x = -1
 			}
 		}
 	}
 }
-
 func animationCycle(snake *Snake) {
 	for {
-		<-time.After(100 * time.Millisecond)
-		snake.MoveSnake()
 		snake.field.DrawField()
 		snake.DrawSnake()
-		snake.field.screen.Show()
+		snake.MoveSnake()
+		(*snake.field.screen).Show()
+		<-time.After(time.Millisecond * snake.delay)
 	}
 }
 
@@ -106,7 +118,7 @@ func drawText(s tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, text string
 	}
 }
 
-func newField(s tcell.Screen, x int, y int, width int, height int, style tcell.Style) Field {
+func newField(s *tcell.Screen, x int, y int, width int, height int, style tcell.Style) Field {
 	var f Field
 	f.x = x
 	f.y = y
@@ -136,18 +148,20 @@ func (c Cell) DrawCell(s tcell.Screen, symbol rune, style tcell.Style) {
 
 func (f Field) DrawField() {
 	for _, c := range f.cells {
-		c.DrawCell(f.screen, '-', f.style)
+		c.DrawCell(*f.screen, '-', f.style)
 	}
 }
 
 func newSnake(f Field, x int, y int, length int, style tcell.Style) Snake {
 	var snake Snake
-	snake.field = f
+	snake.field = &f
 	snake.head = Cell{x, y}
 	snake.tail = make([]Cell, length-1)
 	snake.style = style
 	snake.direction.y = -1
 	snake.direction.x = 0
+	snake.delay = 100
+
 	for i := range snake.tail {
 		snake.tail[i] = Cell{x, y + i + 1}
 	}
@@ -156,9 +170,9 @@ func newSnake(f Field, x int, y int, length int, style tcell.Style) Snake {
 }
 
 func (snake Snake) DrawSnake() {
-	snake.head.DrawCell(snake.field.screen, '0', snake.style)
+	snake.head.DrawCell(*snake.field.screen, '0', snake.style)
 	for _, cell := range snake.tail {
-		cell.DrawCell(snake.field.screen, '0', snake.style)
+		cell.DrawCell(*snake.field.screen, '0', snake.style)
 	}
 }
 
